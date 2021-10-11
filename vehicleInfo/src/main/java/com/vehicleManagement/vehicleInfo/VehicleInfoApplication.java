4b40c3d18a7c706130692755094ab5e0f8e69adf
balacoherent <86341262+balacoherent@users.noreply.github.com>
2021-10-11 20:05:20 +0530

    Add files via upload

---
+++ vehicleInfo/src/main/java/com/vehicleManagement/vehicleInfo/VehicleInfoApplication.java
@@ -0,0 +1,13 @@
+package com.vehicleManagement.vehicleInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleInfoApplication.class, args);
+	}
+
+}
