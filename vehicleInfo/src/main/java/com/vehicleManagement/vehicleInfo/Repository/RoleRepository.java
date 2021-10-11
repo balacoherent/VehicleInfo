f28dbe7f971e5780a64a117e7264fc8dc6916234
balacoherent <86341262+balacoherent@users.noreply.github.com>
2021-10-11 20:03:22 +0530

    Delete vehicleInfo directory

--- vehicleInfo/src/main/java/com/vehicleManagement/vehicleInfo/Repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vehicleManagement.vehicleInfo.Repository;
-
-import com.vehicleManagement.vehicleInfo.Entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-}
--- vehicleInfo/src/main/java/com/vehicleManagement/vehicleInfo/Repository/VehicleTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vehicleManagement.vehicleInfo.Repository;
-
-import com.vehicleManagement.vehicleInfo.Entity.VehicleType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehicleTypeRepository extends JpaRepository<VehicleType, Integer> {
-}
